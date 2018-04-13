d946b98e15f35caa37befd309f057571aa7fc1f9
Thomas Zhu <dev@thomaszhu.com>
2018-04-13 12:35:45 -0700

    Start the compaign contract (request-related methods pending)

---
+++ src/ethereum/contracts/campaign.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.21;
+
+contract Campaign {
+  struct Request {
+    string description;
+    uint value;
+    address recipient;
+    bool complete;
+  }
+
+  address public manager;
+  uint public minimumContribution;
+  address[] public approvers;
+  Request[] public requests;
+
+  modifier restricted() {
+    require(msg.sender == manager);
+    _;
+  }
+
+  function Campaign(uint _minimumContribution) public {
+    manager = msg.sender;
+    minimumContribution = _minimumContribution;
+  }
+
+  function contribute() public payable {
+    require(msg.value > minimumContribution);
+
+    approvers.push(msg.sender);
+  }
+
+  function createRequest(string _description, uint _value, address _recipient) public restricted {
+    
+  }
+
+  function approveRequest() public {
+    
+  }
+
+  function finalizeRequest() public restricted {
+    
+  }
+}
