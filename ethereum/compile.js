8b756596103d348c755c9a05204e5dadee80393f
Thomas Zhu <dev@thomaszhu.com>
2018-04-17 10:29:00 -0700

    Add solidity compiler

---
+++ ethereum/compile.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const contractPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+
+Object.keys(output).forEach((contract) => {
+  fs.outputJsonSync(
+    path.resolve(buildPath, `${contract.replace(':', '')}.json`),
+    output[contract],
+  );
+});
