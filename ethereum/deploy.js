f2248e3583a6d990c20de4c8e8fb44b539db16d9
Thomas Zhu <dev@thomaszhu.com>
2018-04-18 13:16:26 -0700

    Deploy CampaignFactory

---
+++ ethereum/deploy.js
@@ -0,0 +1,31 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+
+const {
+  interface: abi,
+  bytecode,
+} = require('./build/CampaignFactory.json');
+const {
+  mnemonic,
+  infura: { rinkeby },
+} = require('../private/ethereum');
+
+const provider = new HDWalletProvider(
+  mnemonic,
+  rinkeby,
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  const result = await new web3.eth.Contract(JSON.parse(abi))
+    .deploy({ data: bytecode })
+    .send({
+      from: accounts[0],
+      gas: '1000000',
+    });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
