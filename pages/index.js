9e917f3636b329f90c398f68c386a0b517fee38d
Thomas Zhu <dev@thomaszhu.com>
2018-04-20 14:38:38 -0700

    Show campaign addresses on index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {
+  Button,
+  Card,
+} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 import factory from '../ethereum/build/factory';
@@ -10,14 +14,41 @@ class CampaignIndex extends Component {
   }
 
   static propTypes = {
-    campaigns: PropTypes.shape({}).isRequired,
+    campaigns: PropTypes.arrayOf(PropTypes.shape({}.isRequired)),
+  };
+
+  static defaultProps = {
+    campaigns: [],
+  }
+
+  renderCampaigns = () => {
+    const items = this.props.campaigns.map(address => ({
+      header: address,
+      description: <a>View Campaign</a>,
+      fluid: true,
+    }));
+
+    return <Card.Group items={items} />;
   };
 
   render() {
-    const { campaigns } = this.props;
-
     return (
-      <div>{campaigns[0]}</div>
+      <div>
+        {/* TODO: Temporary workaround only */}
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+        />
+
+        <h3>Open Campaigns</h3>
+        {this.renderCampaigns()}
+
+        <Button
+          content="Create Campaign"
+          icon="add"
+          primary
+        />
+      </div>
     );
   }
 }
