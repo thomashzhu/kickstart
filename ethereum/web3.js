0e345762e2ae0a3b037583af0082555acb561301
Thomas Zhu <dev@thomaszhu.com>
2018-04-19 23:40:32 -0700

    Modify web3 to work even metamask is not available

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,18 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+const {
+  infura: { rinkeby },
+} = require('../private/ethereum');
+
+const web3 = (() => {
+  if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // We are in the browser and metamask is running.
+    return new Web3(window.web3.currentProvider);
+  }
+
+  // We are on the server *OR* the user is not running metamask.
+  const provider = new Web3.providers.HttpProvider(rinkeby);
+  return new Web3(provider);
+})();
 
 export default web3;
