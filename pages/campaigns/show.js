44c94e7e76a964e45fa8422d5045c605e86af98b
Thomas Zhu <dev@thomaszhu.com>
2018-04-22 19:36:33 -0700

    Modify and re-deploy the Campaign contract

---
+++ pages/campaigns/show.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Layout from '../../components/Layout';
+
+class CampaignShow extends Component {
+  state = {
+
+  };
+  
+  componentDidMount() {
+
+  }
+
+  render = () => (
+    <Layout>
+      <h3>Campaign Show</h3>
+    </Layout>
+  );
+}
+
+export default CampaignShow;
